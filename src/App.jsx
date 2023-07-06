5faa24ef38465aa8df773ac92c669802ceafeab3
Colson <colsoncalifornication@gmail.com>
2023-07-06 11:43:50 +0545

    create Song playlist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import MoviePlaylist from './components/MoviePlaylist';
+import SongPlaylist from './components/SongPlaylist';
 
 function App() {
   return (
     <div>
       <MoviePlaylist />
+      <hr />
+      <SongPlaylist />
     </div>
   );
 }
---
+++ src/components/SongPlaylist.jsx
@@ -0,0 +1,20 @@
+function SongPlaylist() {
+  // To Do:
+  // Get lists of songs:
+  const songPlaylist = [];
+
+  return (
+    <div>
+      <div className="content">
+        <div className="table-header">
+          <h3 className="subtitle is-3">Song Playlist</h3>
+          <div className="buttons">
+            <button className="button is-link">+ Add Song to Playlist</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SongPlaylist;
