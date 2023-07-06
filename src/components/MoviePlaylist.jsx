07e6d2eec43cb1aa17d0091d4787cd5f8156079a
Colson <colsoncalifornication@gmail.com>
2023-07-06 11:54:48 +0545

    render movies list with map

--- src/components/MoviePlaylist.jsx
+++ src/components/MoviePlaylist.jsx
@@ -1,3 +1,5 @@
+import { createRandomMovie } from '../../data';
+
 function MoviePlaylist() {
   // To Do:
   // Get list of movies
@@ -8,16 +10,37 @@ function MoviePlaylist() {
     // Add movie to list of movies
   };
 
+  const handleMovieRemove = () => {
+    // To Do:
+    // Remove movie from list of movies
+  };
+
+  const renderedMovies = moviePlaylist.map((movie) => {
+    return (
+      <li>
+        {movie}
+        <button
+          onClick={() => handleMovieRemove(movie)}
+          className="button is-danger"
+        ></button>
+      </li>
+    );
+  });
+
   return (
-    <div>
-      <div className="content">
-        <div className="table-header">
-          <h3 className="subtitle is-3">Movie Playlist</h3>
-          <div className="buttons">
-            <button className="button is-link">+ Add Movie to Playlist</button>
-          </div>
+    <div className="content">
+      <div className="table-header">
+        <h3 className="subtitle is-3">Movie Playlist</h3>
+        <div className="buttons">
+          <button
+            onClick={() => handleMovieAdd(createRandomMovie())}
+            className="button is-link"
+          >
+            + Add Movie to Playlist
+          </button>
         </div>
       </div>
+      <ul>{renderedMovies}</ul>
     </div>
   );
 }
