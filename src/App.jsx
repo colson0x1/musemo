ace7f72e77e61aca56031d31543f5cf44352f1e1
Colson <colsoncalifornication@gmail.com>
2023-07-06 12:05:25 +0545

    refractor App

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import MoviePlaylist from './components/MoviePlaylist';
 import SongPlaylist from './components/SongPlaylist';
 
 function App() {
+  const handleResetClick = () => {
+    //
+  };
+
   return (
-    <div>
+    <div className="container is-fluid">
+      <button onClick={() => handleResetClick()} className="button is-danger">
+        Reset Both Playlists
+      </button>
+      <hr />
       <MoviePlaylist />
       <hr />
       <SongPlaylist />
